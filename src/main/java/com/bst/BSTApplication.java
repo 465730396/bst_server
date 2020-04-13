68a2913eccf858d4fb3374fabd95a701906270c2
Alex <lilei_happyboy@163.com>
2020-04-13 10:34:05 +0800

    初始化springboot

--- src/main/java/com/bst/BSTApplication.java
+++ src/main/java/com/bst/BSTApplication.java
@@ -2,6 +2,8 @@ package com.bst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class BSTApplication {
@@ -9,5 +11,4 @@ public class BSTApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BSTApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/bst/car/violation/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.bst.car.violation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class TestController {
+
+    //hello world :)
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
