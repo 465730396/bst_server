95dda65f5342f34fbc5dd154b9f024673e1cfae2
Alex <lilei_happyboy@163.com>
2020-04-13 09:56:10 +0800

    初始化springboot

---
+++ src/main/java/com/bst/BSTApplication.java
@@ -0,0 +1,13 @@
+package com.bst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSTApplication.class, args);
+	}
+
+}
