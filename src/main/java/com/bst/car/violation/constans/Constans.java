63d7d12865c406f76472ba10f134044d4d0bdf6a
Alex <lilei_happyboy@163.com>
2020-04-19 00:10:12 +0800

    增加违章code

--- src/main/java/com/bst/car/violation/constans/Constans.java
+++ src/main/java/com/bst/car/violation/constans/Constans.java
@@ -1,11 +1,15 @@
 package com.bst.car.violation.constans;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Constans {
 
     public static Map<Integer,String> codesMap = new HashMap<>();
+    public static Set<Integer> daijiaoCodes  = new HashSet<>();
+
     static {
         codesMap.put(201,"交管局为空");
         codesMap.put(202,"交管局不存在");
@@ -23,5 +27,21 @@ public class Constans {
         codesMap.put(220,"交管局服务器错误");
         codesMap.put(230,"没有信息");
         codesMap.put(217,"有违章，详情请到交管网查询");
+
+
+
+        //支持代缴的code 第二位是扣得分
+        daijiaoCodes.add(7020);
+        daijiaoCodes.add(1039);
+        daijiaoCodes.add(1091);
+        daijiaoCodes.add(1043);
+        daijiaoCodes.add(1019);
+        daijiaoCodes.add(1044);
+        daijiaoCodes.add(1087);
+        daijiaoCodes.add(1090);
+        daijiaoCodes.add(6023);
+        daijiaoCodes.add(1018);
+        daijiaoCodes.add(10399);//
+        daijiaoCodes.add(10789);
     }
 }
