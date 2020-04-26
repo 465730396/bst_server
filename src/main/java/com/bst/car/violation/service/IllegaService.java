40f812be6598a317cb83e72f6ef115e3da0b2fb6
Alex <lilei_happyboy@163.com>
2020-04-26 12:31:22 +0800

    清理文件

--- src/main/java/com/bst/car/violation/service/IllegaService.java
+++
@@ -1,8 +0,0 @@
-package com.bst.car.violation.service;
-
-import com.bst.car.violation.entity.CarInfo;
-import com.bst.car.violation.vo.CommonResponse;
-
-public interface  IllegaService {
-    public CommonResponse queryCarIllega(CarInfo carInfo);
-}
